9457ac7f008f9654d293e9320738b5c032dc251a
yacinedjaou <yacinedjaou@cmail.carleton.ca>
2024-09-18 17:40:41 -0400

    New Buddy created Buddy added and removed from AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,7 +5,10 @@ public class AddressBook {
     ArrayList<BuddyInfo> buddies = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Yacine","Djaou","Ottawa","2342342342","sdahfjkl@gmail.com");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy("Yacine","Djaou");
     }
 
     public void addBuddy(BuddyInfo buddy){
